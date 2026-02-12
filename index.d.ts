ff2465b807436a5b01a7f1a8dc45456beaf40934
Signor_P <124287199+SignorMassimo@users.noreply.github.com>
2026-02-12 22:13:42 +0400

    Release 0.0.3 sync

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export declare class NexLog {
+    static nex(...args: any[]): void;
+    static log(...args: any[]): void;
+    static info(...args: any[]): void;
+    static error(...args: any[]): void;
+    static warn(...args: any[]): void;
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+import { blue, gray, red, yellow } from 'kleur/colors';
+export class NexLog {
+    static nex(...args) {
+        console.log(gray('[NEX]'), ...args);
+    }
+    static log(...args) {
+        console.log(gray('[LOG]'), ...args);
+    }
+    static info(...args) {
+        console.log(blue('[INFO]'), ...args);
+    }
+    static error(...args) {
+        console.log(red('[ERROR]'), ...args);
+    }
+    static warn(...args) {
+        console.log(yellow('[WARN]'), ...args);
+    }
+}
